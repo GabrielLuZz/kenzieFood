c1a5d5d3c62ad087597bd5788c81e874c38e4053
Saamu192 <samuelpr192@gmail.com>
2022-05-17 11:21:42 -0300

    fix Login

--- src/Js/handlers/AuthHandler.js
+++ src/Js/handlers/AuthHandler.js
@@ -2,7 +2,7 @@ import { Authentication } from "../controllers/authController.js";
 import { UserService } from "../services/UserService.js";
 
 const login = async (data) => {
-  const user = await Api.login(data);
+  const user = await UserService.login(data);
   if (user.error !== undefined) {
     return false;
   }
