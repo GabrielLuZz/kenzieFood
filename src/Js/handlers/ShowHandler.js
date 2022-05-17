b00d38550d660865ccaf5ae934096b666b832308
Pedro26fer <p2698fernandes@gmail.com>
2022-05-17 13:10:00 -0300

    fix: function-filter

--- src/Js/handlers/ShowHandler.js
+++ src/Js/handlers/ShowHandler.js
@@ -1,8 +1,26 @@
-export class Filter{
+export class ShowHandler{
     static filterPerCategory(products,category){
-        const filterProducts = products.filter((product) => product.categoria === category)
+        const filterProducts = products.filter((product) => product.categoria.toLowerCase() === category.toLowerCase())
         return filterProducts
     }
+
+    static searchedProducts(texto,products){
+        const searchedProducts = []
+        products.forEach(product => {
+            if(product.nome.toLowerCase().includes(texto)){
+                searchedProducts.push(product)
+            }
+            else if (product.categoria.toLowerCase().includes(texto)){
+                searchedProducts.push(product)
+            }
+            else if (product.descricao.toLowerCase().includes(texto)){
+                searchedProducts.push(product)
+            }
+            return searchedProducts
+        });
+
+    }
+
 }
 
 
