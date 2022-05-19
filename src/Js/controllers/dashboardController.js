bb31254f17a5a8c9dfdbd14e361ea865d740849e
Júlio César Oliveira <juliocomp99@hotmail.com>
2022-05-19 12:35:07 -0300

    Animation Pop-up DashBoard - Clear DashboardController

--- src/Js/controllers/dashboardController.js
+++ src/Js/controllers/dashboardController.js
@@ -7,21 +7,3 @@ import { DashboardHandler } from '../handlers/DashBoardHandler.js';
 const verify = await AuthHandler.userVerify();
 
 ShowHandler.headerMain(verify);
-
-const registerProduct = document.getElementById("open");
-registerProduct.addEventListener("click", () => { DashboardHandler.addClassList("modal__cadastrar--edit") });
-
-const closeModalButton = document.getElementById("modal__close");
-closeModalButton.addEventListener("click", () => { DashboardHandler.removeClassList("modal__cadastrar--edit") });
-
-const openModalEdit = document.getElementById("btn__edit");
-openModalEdit.addEventListener("click", () => { DashboardHandler.addClassList("modal__cadastrar--edit") });
-
-const openRemoveProduct = document.getElementById("modal__close");
-openRemoveProduct.addEventListener("click", () => { DashboardHandler.removeClassList("modal__cadastrar--edit") });
-
-const openModalRemove = document.getElementById("btn__remove");
-openModalRemove.addEventListener("click", () => { DashboardHandler.addClassList("modal__cadastrar--edit") });
-
-const closeModalRemove = document.getElementById("modal__close");
-closeModalRemove.addEventListener("click", () => { DashboardHandler.removeClassList("modal__cadastrar--edit") });
