2c6726a996bb65b534b945cc3fa64fb965c080de
Gabriel Luz <gabrielluzsantosl@gmail.com>
2022-05-19 20:21:55 -0300

    resolving problems relationed to the conflicts resolved on git hub

--- src/Js/controllers/dashboardController.js
+++ src/Js/controllers/dashboardController.js
@@ -7,9 +7,6 @@ import { DashBoardHandler } from '../handlers/DashBoardHandler.js';
 import { ProductService } from '../services/ProductService.js';
 import { WarningHandler } from '../handlers/WarningHandler.js';
 
-
-import { DashBoardHandler } from "../handlers/DashBoardHandler.js";
-
 const verify = await AuthHandler.userVerify();
 
 if (verify) {
@@ -20,7 +17,7 @@ if (verify) {
 
     const products = await ProductService.getPrivateProducts(localStorage.getItem('Token'));
     console.log(products)
-    DashBoardHandler.listProductsinDashboard(products)
+    DashBoardHandler.listProductsInDashboard(products)
 
     const filters = document.querySelectorAll('.menu__item');
     const rightSide__field = document.querySelector('.rightSide__field');
@@ -36,7 +33,7 @@ if (verify) {
 
             const filtered = ShowHandler.filterPerCategory(products, category);
 
-            DashBoardHandler.listProductsinDashboard(filtered)
+            DashBoardHandler.listProductsInDashboard(filtered)
             ShowHandler.changeTheSelected(event.currentTarget)
         })
     })
@@ -47,64 +44,63 @@ if (verify) {
 
         if (text) {
             const searched = ShowHandler.searchedProducts(text, products);
-            DashBoardHandler.listProductsinDashboard(searched);
+            DashBoardHandler.listProductsInDashboard(searched);
         } else {
-            DashBoardHandler.listProductsinDashboard(products)
+            DashBoardHandler.listProductsInDashboard(products)
         }
     })
 
-    open.addEventListener('click', () => {
-        aside.style.opacity = 0;
+    // open.addEventListener('click', () => {
+    //     aside.style.opacity = 0;
 
-        aside.style.display = "flex";
+    //     aside.style.display = "flex";
 
-        setTimeout(() => {
+    //     setTimeout(() => {
 
-            aside.style.opacity = 1;
+    //         aside.style.opacity = 1;
 
-        }, 200);
-    })
-
-
+    //     }, 200);
+    // })
 
 
-DashBoardHandler.listProductsinDashboard();
 
-const modal__close = document.querySelector("#modal__close");
-modal__close.addEventListener("click", (event) => {
-  event.preventDefault();
-  DashBoardHandler.closeModal();
-});
 
-const modal__forfeit = document.querySelector("#modal__forfeit");
-modal__forfeit.addEventListener("click", (event) => {
-  event.preventDefault();
-  DashBoardHandler.closeModal();
-});
+    DashBoardHandler.listProductsInDashboard();
 
-const modal__save = document.querySelector("#modal__save");
-modal__save.addEventListener("click", (event) => {
-  event.preventDefault;
-  DashBoardHandler.sendModal(event);
-});
 
-const productTagAll = document.querySelectorAll(
-  ".modal__field.modal__field--radio"
-);
-productTagAll.forEach((tag) => {
-  tag.addEventListener("click", (event) => {
-    event.preventDefault();
-    DashBoardHandler.changeTag(event, productTagAll);
-  });
-});
+    modal__close.addEventListener("click", (event) => {
+        event.preventDefault();
+        DashBoardHandler.closeModal();
+    });
 
-const add__newProduct = document.querySelector(".open");
-add__newProduct.addEventListener("click", (event) => {
-  event.preventDefault();
-  DashBoardHandler.modalDashboard(event);
-});
+    const modal__forfeit = document.querySelector("#modal__forfeit");
+    modal__forfeit.addEventListener("click", (event) => {
+        event.preventDefault();
+        DashBoardHandler.closeModal();
+    });
+
+    const modal__save = document.querySelector("#modal__save");
+    modal__save.addEventListener("click", (event) => {
+        event.preventDefault;
+        DashBoardHandler.sendModal(event);
+    });
+
+    const productTagAll = document.querySelectorAll(
+        ".modal__field.modal__field--radio"
+    );
+    productTagAll.forEach((tag) => {
+        tag.addEventListener("click", (event) => {
+            event.preventDefault();
+            DashBoardHandler.changeTag(event, productTagAll);
+        });
+    });
+
+    const add__newProduct = document.querySelector(".open");
+    add__newProduct.addEventListener("click", (event) => {
+        event.preventDefault();
+        DashBoardHandler.modalDashboard(event);
+    });
 
 } else {
     location.href = '../../index.html'
 }
-
--- src/Js/handlers/DashBoardHandler.js
+++ src/Js/handlers/DashBoardHandler.js
@@ -1,19 +1,24 @@
 import { ProductService } from "../services/ProductService.js";
 
 export class DashBoardHandler {
-  static addClassList(id) {
-    const modal = document.getElementById(id);
-    modal.classList.add("mostrar");
-  }
+    static addClassList(id) {
+        const modal = document.getElementById(id);
+        modal.classList.add("mostrar");
+    }
 
-  static removeClassList(id) {
-    const modal = document.getElementById(id);
-    modal.classList.remove("mostrar");
-  }
+    static removeClassList(id) {
+        const modal = document.getElementById(id);
+        modal.classList.remove("mostrar");
+    }
 
 
-  static listProductsinDashboard(privateProducts) {
-      const mainBody = document.querySelector('.body');
+    static async listProductsInDashboard(privateProducts) {
+
+        if (!privateProducts) {
+            privateProducts = await ProductService.getPrivateProducts(localStorage.getItem('Token'));
+        }
+
+        const mainBody = document.querySelector('.body');
         mainBody.innerText = '';
 
         privateProducts.forEach(product => {
@@ -30,148 +35,147 @@ export class DashBoardHandler {
             const card__btnRemove = document.createElement('button');
 
 
-      card.classList.add("card");
-      card__product.classList.add("card__product");
-      card__image.classList.add("card__image");
-      card__image.src = product.imagem;
-      card__image.alt = product.nome;
-      card__title.classList.add("card__title");
-      card__title.innerText = product.nome;
-      card__categories.classList.add("card__categories");
-      card__category.classList.add("card__category");
-      card__category.innerText = product.categoria;
-      card__description.classList.add("card__description");
-      card__text.classList.add("card__text");
-      card__text.innerText = product.descricao;
-      card__actions.classList.add("card__actions");
-      card__btnEdit.classList.add("card__btn");
-      card__btnEdit.classList.add("card__btn--edit");
-      card__btnEdit.innerHTML =
-        '<img src="../assets/images/edit.png" alt="edit image">';
-      card__btnRemove.classList.add("card__btn");
-      card__btnRemove.classList.add("card__btn--remove");
-      card__btnRemove.innerHTML =
-        '<img src="../assets/images/trash.png" alt="trash image">';
+            card.classList.add("card");
+            card__product.classList.add("card__product");
+            card__image.classList.add("card__image");
+            card__image.src = product.imagem;
+            card__image.alt = product.nome;
+            card__title.classList.add("card__title");
+            card__title.innerText = product.nome;
+            card__categories.classList.add("card__categories");
+            card__category.classList.add("card__category");
+            card__category.innerText = product.categoria;
+            card__description.classList.add("card__description");
+            card__text.classList.add("card__text");
+            card__text.innerText = product.descricao;
+            card__actions.classList.add("card__actions");
+            card__btnEdit.classList.add("card__btn");
+            card__btnEdit.classList.add("card__btn--edit");
+            card__btnEdit.innerHTML =
+                '<img src="../assets/images/edit.png" alt="edit image">';
+            card__btnRemove.classList.add("card__btn");
+            card__btnRemove.classList.add("card__btn--remove");
+            card__btnRemove.innerHTML =
+                '<img src="../assets/images/trash.png" alt="trash image">';
 
-      card__btnEdit.addEventListener("click", (event) => {
-        this.modalDashboard(event, product);
-      });
+            card__btnEdit.addEventListener("click", (event) => {
+                this.modalDashboard(event, product);
+            });
 
-      card__btnRemove.addEventListener("click", (event) => {
-        this.modalDashboard(event, product);
-      });
+            card__btnRemove.addEventListener("click", (event) => {
+                this.modalDashboard(event, product);
+            });
 
-      card.append(
-        card__product,
-        card__categories,
-        card__description,
-        card__actions
-      );
-      card__product.append(card__image, card__title);
-      card__categories.appendChild(card__category);
-      card__description.appendChild(card__text);
-      card__actions.append(card__btnEdit, card__btnRemove);
+            card.append(
+                card__product,
+                card__categories,
+                card__description,
+                card__actions
+            );
+            card__product.append(card__image, card__title);
+            card__categories.appendChild(card__category);
+            card__description.appendChild(card__text);
+            card__actions.append(card__btnEdit, card__btnRemove);
 
-      mainBody.appendChild(card);
-    });
-  }
-  static modalDashboard(event, product) {
-    const token = localStorage.getItem("Token");
-    const btn__class = event.path[1].classList[1];
-    if (btn__class === "card__btn--edit") {
-      const modal = document.querySelector(".modal__container");
-      modal.style.display = "flex";
-      const productName = document.querySelector("#modal__title");
-      const productDescription = document.querySelector("#modal__description");
-      const productPrice = document.querySelector("#modal__price");
-      const productImg = document.querySelector("#modal__image");
-      const productContainer = document.querySelector(".modal");
-      const modalTitle = document.querySelector("#modal__title--main");
-      modalTitle.innerText = "Edição de Produtos";
-      productContainer.id = product.id;
-      const productTagAll = document.querySelectorAll(
-        ".modal__field.modal__field--radio"
-      );
-      const productTag = [...productTagAll];
-      const tagToLower = product.categoria.toLowerCase();
-      productTag.forEach((element) => {
-        element.classList.remove("modal__field--salmon");
-        if (element.id === tagToLower) {
-          element.classList.add("modal__field--salmon");
+            mainBody.appendChild(card);
+        });
+    }
+    static async modalDashboard(event, product) {
+        const token = localStorage.getItem("Token");
+        const btn__class = event.path[1].classList[1];
+        if (btn__class === "card__btn--edit") {
+            const modal = document.querySelector(".modal__container");
+            modal.style.display = "flex";
+            const productName = document.querySelector("#modal__title");
+            const productDescription = document.querySelector("#modal__description");
+            const productPrice = document.querySelector("#modal__price");
+            const productImg = document.querySelector("#modal__image");
+            const productContainer = document.querySelector(".modal");
+            const modalTitle = document.querySelector("#modal__title--main");
+            modalTitle.innerText = "Edição de Produtos";
+            productContainer.id = product.id;
+            const productTagAll = document.querySelectorAll(
+                ".modal__field.modal__field--radio"
+            );
+            const productTag = [...productTagAll];
+            const tagToLower = product.categoria.toLowerCase();
+            productTag.forEach((element) => {
+                element.classList.remove("modal__field--salmon");
+                if (element.id === tagToLower) {
+                    element.classList.add("modal__field--salmon");
+                }
+            });
+            productImg.value = product.imagem;
+            productPrice.value = product.preco;
+            productDescription.value = product.descricao;
+            productName.value = product.nome;
+        } else if (btn__class === "card__btn--remove") {
+            await ProductService.deleteProduct(token, product.id);
+            this.listProductsInDashboard();
+        } else {
+            const modalTitle = document.querySelector("#modal__title--main");
+            modalTitle.innerText = "Adicionar novo Produto";
+            const modal = document.querySelector(".modal__container");
+            modal.style.display = "flex";
+            const productName = document.querySelector("#modal__title");
+            const productDescription = document.querySelector("#modal__description");
+            const productPrice = document.querySelector("#modal__price");
+            const productImg = document.querySelector("#modal__image");
+
+            productImg.placeholder = "URL Da imagem do produto";
+            productPrice.value = 0;
+            productDescription.placeholder = "Adicione uma descrição do seu produto";
+            productName.placeholder = "Nome do Produto";
         }
-      });
-      productImg.value = product.imagem;
-      productPrice.value = product.preco;
-      productDescription.value = product.descricao;
-      productName.value = product.nome;
-    } else if (btn__class === "card__btn--remove") {
-      ProductService.deleteProduct(token, product.id);
-      this.listProductsinDashboard();
-    } else {
-      const modalTitle = document.querySelector("#modal__title--main");
-      modalTitle.innerText = "Adicionar novo Produto";
-      const modal = document.querySelector(".modal__container");
-      modal.style.display = "flex";
-      const productName = document.querySelector("#modal__title");
-      const productDescription = document.querySelector("#modal__description");
-      const productPrice = document.querySelector("#modal__price");
-      const productImg = document.querySelector("#modal__image");
 
-      productImg.placeholder = "URL Da imagem do produto";
-      productPrice.value = 0;
-      productDescription.placeholder = "Adicione uma descrição do seu produto";
-      productName.placeholder = "Nome do Produto";
+    }
+    static closeModal() {
+        const modal = document.querySelector(".modal__container");
+        modal.style.display = "none";
+    }
+    static async sendModal(event) {
+        const productId = event.path[2].id;
+        const main__event = event.path[2].childNodes[3].childNodes[1];
+        const data__base = [...main__event];
+        const token = localStorage.getItem("Token");
+        let newTag = "";
+        const tag = document.querySelectorAll(".modal__field.modal__field--radio");
+        tag.forEach((element) => {
+            if (element.classList[3] === "modal__field--salmon") {
+                newTag = element.innerText;
+            }
+        });
+        const data__array = [];
+        data__base.forEach((element) => {
+            data__array.push(element.value);
+        });
+        let newValues = {
+            nome: data__array[0],
+            preco: data__array[2],
+            categoria: newTag,
+            descricao: data__array[1],
+            imagem: data__array[3],
+        };
+        console.log(newValues);
+        const modalTitle = document.querySelector("#modal__title--main");
+        if (modalTitle.innerText === "Edição de Produtos") {
+            await ProductService.editProduct(token, newValues, productId);
+            this.closeModal();
+            this.listProductsInDashboard();
+        } else {
+            await ProductService.createProduct(token, newValues);
+            this.closeModal();
+            this.listProductsInDashboard();
+        }
     }
 
-  }
-  static closeModal() {
-    const modal = document.querySelector(".modal__container");
-    modal.style.display = "none";
-  }
-  static async sendModal(event) {
-    const productId = event.path[2].id;
-    const main__event = event.path[2].childNodes[3].childNodes[1];
-    const data__base = [...main__event];
-    const token = localStorage.getItem("Token");
-    let newTag = "";
-    const tag = document.querySelectorAll(".modal__field.modal__field--radio");
-    tag.forEach((element) => {
-      if (element.classList[3] === "modal__field--salmon") {
-        newTag = element.innerText;
-      }
-    });
-    const data__array = [];
-    data__base.forEach((element) => {
-      data__array.push(element.value);
-    });
-    let newValues = {
-      nome: data__array[0],
-      preco: data__array[2],
-      categoria: newTag,
-      descricao: data__array[1],
-      imagem: data__array[3],
-    };
-    console.log(newValues);
-    const modalTitle = document.querySelector("#modal__title--main");
-    if (modalTitle.innerText === "Edição de Produtos") {
-      await ProductService.editProduct(token, newValues, productId);
-      this.closeModal();
-      this.listProductsinDashboard();
-    } else {
-      await ProductService.createProduct(token, newValues);
-      this.closeModal();
-      this.listProductsinDashboard();
+    static changeTag(event, tags) {
+        const target = event.target;
+        const newA = tags.forEach((element) => {
+            if (element.classList[3] === "modal__field--salmon") {
+                element.classList.remove("modal__field--salmon");
+            }
+            target.classList.add("modal__field--salmon");
+        });
     }
-  }
-
-  static changeTag(event, tags) {
-    const target = event.target;
-    const newA = tags.forEach((element) => {
-      if (element.classList[3] === "modal__field--salmon") {
-        element.classList.remove("modal__field--salmon");
-      }
-      target.classList.add("modal__field--salmon");
-    });
-  }
 }
-
