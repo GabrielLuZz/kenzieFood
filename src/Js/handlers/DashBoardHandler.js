2dcb33944ac1cb5e57465a6f57b443c417fa346c
Saamu192 <samuelpr192@gmail.com>
2022-05-19 11:51:05 -0300

    feature
    the base of that new function has been delcared an not function at all, waiting for eventlistener

--- src/Js/handlers/DashBoardHandler.js
+++ src/Js/handlers/DashBoardHandler.js
@@ -9,11 +9,27 @@ export class DashboardHandler {
     modal.classList.remove("mostrar");
   }
 
-  static modalDashboard(event, data) {}
+  static modalDashboard(event, data) {
+    console.log(event);
+    const modal = document.querySelector("aside");
+    modal.style.display = "flex";
+    // if (event) {
+    //   this.modalEditProduct(data);
+    // } else if (event) {
+    //   this.modalCrate(event);
+    // } else if (event) {
+    //   this.modalDelete(event, data);
+    // }
+  }
 
   static modalEditProduct(data) {}
 
   static modalDelete(event, data) {}
 
   static modalCrate(event) {}
+
+  static closeModal(event) {
+    const modal = document.querySelector("aside");
+    modal.style.display = "none";
+  }
 }
