b9e73ce7c569d2ea7a69b4e0ac2f17abd9da0b94
Saamu192 <samuelpr192@gmail.com>
2022-05-19 11:33:46 -0300

    fix name of class
    the class mostrar has been changed to DashboardHandler

--- src/Js/handlers/DashBoardHandler.js
+++ src/Js/handlers/DashBoardHandler.js
@@ -1,11 +1,19 @@
-export class Mostrar {
-    static addClassList(id){
+export class DashboardHandler {
+  static addClassList(id) {
     const modal = document.getElementById(id);
     modal.classList.add("mostrar");
-}
+  }
 
-    static removeClassList (id){
+  static removeClassList(id) {
     const modal = document.getElementById(id);
     modal.classList.remove("mostrar");
-}
+  }
+
+  static modalDashboard(event, data) {}
+
+  static modalEditProduct(data) {}
+
+  static modalDelete(event, data) {}
+
+  static modalCrate(event) {}
 }
